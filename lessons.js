fbc47534a3b0f92a6daab3d220ce6e98c2aaf373
MeMymine1 <99880090+MeMymine1@users.noreply.github.com>
2024-05-16 11:01:12 +0400

    Add files via upload

---
+++ lessons.js
@@ -0,0 +1,99 @@
+let lessons = [
+    {
+        id: 1,
+        subject: "Mathematics",
+        location: "Room 101",
+        price: 30,
+        spaces: 5,
+        icon: "fa-calculator"
+    },
+    {
+        id: 2,
+        subject: "Science",
+        location: "Lab 202",
+        price: 35,
+        spaces: 5,
+        icon: "fa-flask"
+    },
+    {
+        id: 3,
+        subject: "Art",
+        location: "Art Studio",
+        price: 25,
+        spaces: 5,
+        icon: "fa-paint-brush"
+    },
+    {
+        id: 4,
+        subject: "Music",
+        location: "Music Room",
+        price: 40,
+        spaces: 5,
+        icon: "fa-music"
+    },
+    {
+        id: 5,
+        subject: "Coding",
+        location: "Computer Lab",
+        price: 45,
+        spaces: 5,
+        icon: "fa-laptop-code"
+    },
+    {
+        id: 6,
+        subject: "Football",
+        location: "Soccer Field",
+        price: 20,
+        spaces: 5,
+        icon: "fa-futbol"
+    },
+    {
+        id: 7,
+        subject: "Dance",
+        location: "Dance Studio",
+        price: 30,
+        spaces: 5,
+        icon: "fa-heart"
+    },
+    {
+        id: 8,
+        subject: "Cooking",
+        location: "Home Economics Room",
+        price: 35,
+        spaces: 5,
+        icon: "fa-utensils"
+    },
+    {
+        id: 9,
+        subject: "Photography",
+        location: "Photography Studio",
+        price: 40,
+        spaces: 5,
+        icon: "fa-camera"
+    },
+    {
+        id: 10,
+        subject: "Chess",
+        location: "Chess Room",
+        price: 25,
+        spaces: 5,
+        icon: "fa-chess"
+    },
+    {
+        id: 11,
+        subject: "Language",
+        location: "Language Lab",
+        price: 35,
+        spaces: 5,
+        icon: "fa-language"
+    },
+    {
+        id: 12,
+        subject: "Theater",
+        location: "Theater Room",
+        price: 30,
+        spaces: 5,
+        icon: "fa-theater-masks"
+    }
+];
+
